10596407915f94aee30438783abe3dbd60ce7266
Maira <maira212.a@gmail.com>
2025-09-03 11:57:36 +0000

    Geneardor de Cartas Aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let carta = document.getElementById('myCard')
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let cardSuits = ['♦', '♥', '♠', '♣'];
+  let values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K'];
+
+
+  let randomCardSuits = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+  let randomValues = values[Math.floor(Math.random() * values.length)];
+
+
+  let superiorSuit = document.querySelector(".arriba")
+  superiorSuit.textContent = randomCardSuits;
+
+  let inferiorSuit = document.querySelector(".voltear")
+  inferiorSuit.textContent = randomCardSuits;
+
+  let centerValue = document.querySelector(".valor")
+  centerValue.textContent = randomValues;
+
+
+  if (randomCardSuits == '♦' || randomCardSuits == '♥') {
+    carta.style.color = "red";
+  } else {
+    carta.style.color = "black";
+  }
+ 
+console.log(randomCardSuits);
+console.log(randomValues);
+}
+
